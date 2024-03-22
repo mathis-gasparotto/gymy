206208de1b82fdc76af99d35cabc8077ad60c614
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2024-03-22 09:08:10 +0100

    :sparkles: Add hello username

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,21 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/icon.png" style="width: 200px">
+  <q-page class="h-100">
+    <div class="page-content flex items-center column">
+      <h1 class="text-h4">Bonjour {{ user.username }}</h1>
+      <img alt="Quasar logo" src="~assets/icon.png" style="width: 200px">
+    </div>
   </q-page>
 </template>
 
 <script>
+import { getUser } from 'src/services/userService'
+
 export default {
   name: 'IndexPage',
+  setup() {
+    return {
+      user: getUser()
+    }
+  }
 }
 </script>
