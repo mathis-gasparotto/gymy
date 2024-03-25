7e1b1a34f7ce7516c906327f8a7a01c338c0ef66
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2024-03-25 19:40:13 +0100

    :sparkles: Plan week workouts

---
+++ src/components/Plans/PlanWorkouts.vue
@@ -0,0 +1,298 @@
+<template>
+  <div class="flex column">
+    <GymyHeader :text="plan.label + ' - Entrainements'" />
+    <div v-if="planWorkouts && planWorkouts.length > 0">
+      <div class="relative">
+        <div class="plan-template absolute">
+          <div class="q-mb-lg" v-for="(workout, index) in planWorkouts" :key="workout.id">
+            <h2 class="q-mb-sm text-center text-h5">{{ getDayByIndex(index) }}</h2>
+            <q-card flat class="flex-center column q-px-md plan-template__card">
+              <q-card-section>
+                <div class="text-h6 text-center color-primary flex flex-center">
+                  Repos
+                  <q-btn flat round color="primary" inversed icon="add_circle" @click="remove(index)" />
+                </div>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+        <draggable :list="planWorkouts" class="list-group" ghost-class="ghost" itemKey="idForList" handle=".draggable-btn" @end="onDragEnd" @start="drag=true">
+          <template #item="{ element, index }">
+            <div class="q-mb-lg">
+              <h2 class="q-mb-sm text-center text-h5 workout-day no-hover">{{ getDayByIndex(index) }}</h2>
+              <q-card class="flex-center column q-px-md" v-if="!element.restDay">
+                <q-card-section class="gt-xs">
+                  <div class="text-h6 text-center">
+                    {{ element.label }}
+                  </div>
+                </q-card-section>
+                <q-card-section class="q-pb-none lt-sm">
+                  <div class="text-h6 text-center">
+                    {{ element.label }}
+                  </div>
+                </q-card-section>
+                <q-card-actions horizontal class="absolute-right gt-xs no-wrap">
+                  <q-btn flat round color="negative" icon="fa-solid fa-circle-minus" @click="remove(index)" />
+                </q-card-actions>
+                <q-card-actions horizontal class="lt-sm no-wrap q-pa-none">
+                  <q-btn flat round color="negative" icon="fa-solid fa-circle-minus" @click="remove(index)" />
+                </q-card-actions>
+                <div class="draggable-btn-container">
+                  <q-icon :class="'draggable-btn ' + (drag ? 'cursor-grabbing' : 'cursor-grab')" size="sm" name="menu"></q-icon>
+                </div>
+              </q-card>
+              <q-card class="flex-center column q-px-md plan-template__card restDay cursor-pointer" v-else @click="showAddPlanFunction(index)">
+                <q-card-section>
+                </q-card-section>
+              </q-card>
+            </div>
+          </template>
+        </draggable>
+      </div>
+    </div>
+    <span v-else class="text-center">Aucun entrainement de disponible</span>
+    <q-dialog v-model="showAddPlan">
+      <q-card class="q-px-xs q-py-xs">
+        <q-card-section class="text-h5 text-center">
+          Choisissez un entrainement
+        </q-card-section>
+        <q-card-section>
+          <q-card class="cursor-pointer q-mb-md" v-for="workout in workouts" :key="workout.id" @click="selectWorkout(workout)">
+            <q-card-section class="text-h6 text-center">
+              {{ workout.label }}
+            </q-card-section>
+          </q-card>
+        </q-card-section>
+        <q-card-actions align="center">
+          <q-btn label="Annuler" color="negative" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+import { getWorkouts } from 'src/services/workoutService'
+import { getPlanWorkouts, updatePlan } from 'src/services/planService'
+import GymyHeader from 'src/components/GymyHeader.vue'
+import { errorNotify, successNotify } from 'src/helpers/notifyHelper'
+import draggable from 'vuedraggable'
+import { uid } from 'quasar'
+
+export default {
+  name: 'PlanWorkouts',
+  props: {
+    plan: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    GymyHeader,
+    draggable
+  },
+  data() {
+    return {
+      drag: false,
+      workouts: {},
+      addForm: false,
+      addLoading: false,
+      editForm: false,
+      editLoading: false,
+      workoutToEdit: {},
+      planWorkouts: [],
+      showAddPlan: false,
+      indexToAddWorkout: null
+    }
+  },
+  created() {
+    this.workouts = getWorkouts()
+    this.loadPlanWorkouts()
+  },
+  methods: {
+    showAddPlanFunction(index) {
+      this.indexToAddWorkout = index
+      this.showAddPlan = true
+    },
+    getDayByIndex(index) {
+      switch (index) {
+        case 0:
+          return 'Lundi'
+        case 1:
+          return 'Mardi'
+        case 2:
+          return 'Mercredi'
+        case 3:
+          return 'Jeudi'
+        case 4:
+          return 'Vendredi'
+        case 5:
+          return 'Samedi'
+        case 6:
+          return 'Dimanche'
+      }
+    },
+    onDragEnd(e) {
+      this.drag = false
+      updatePlan(this.plan.id, {
+        mondayWorkoutId: this.planWorkouts[0].restDay ? null : this.planWorkouts[0].id,
+        tuesdayWorkoutId: this.planWorkouts[1].restDay ? null : this.planWorkouts[1].id,
+        wednesdayWorkoutId: this.planWorkouts[2].restDay ? null : this.planWorkouts[2].id,
+        thursdayWorkoutId: this.planWorkouts[3].restDay ? null : this.planWorkouts[3].id,
+        fridayWorkoutId: this.planWorkouts[4].restDay ? null : this.planWorkouts[4].id,
+        saturdayWorkoutId: this.planWorkouts[5].restDay ? null : this.planWorkouts[5].id,
+        sundayWorkoutId: this.planWorkouts[6].restDay ? null : this.planWorkouts[6].id,
+      })
+      .catch((err) => {
+        errorNotify('Une erreur est survenue lors du déplacement de votre entrainement')
+        this.loadPlanWorkouts()
+      })
+    },
+    loadPlanWorkouts() {
+      this.planWorkouts = getPlanWorkouts(this.plan.id)
+      this.planWorkouts = this.planWorkouts.map((planWorkout) => {
+        if (!planWorkout) {
+          return {
+            idForList: uid(),
+            restDay: true,
+            label: 'Repos'
+          }
+        } else {
+          return {
+            ...planWorkout,
+            idForList: uid()
+          }
+        }
+      })
+    },
+    remove(index) {
+      let payload = {}
+      switch (index) {
+        case 0:
+          payload = {
+            mondayWorkoutId: null
+          }
+          break
+        case 1:
+          payload = {
+            tuesdayWorkoutId: null
+          }
+          break
+        case 2:
+          payload = {
+            wednesdayWorkoutId: null
+          }
+          break
+        case 3:
+          payload = {
+            thursdayWorkoutId: null
+          }
+          break
+        case 4:
+          payload = {
+            fridayWorkoutId: null
+          }
+          break
+        case 5:
+          payload = {
+            saturdayWorkoutId: null
+          }
+          break
+        case 6:
+          payload = {
+            sundayWorkoutId: null
+          }
+          break
+      }
+      updatePlan(this.plan.id, payload).then(() => {
+        this.loadPlanWorkouts()
+      }).catch((err) => {
+        errorNotify('Une erreur est survenue lors de le retrait de votre entrainement au programme')
+        this.loadPlanWorkouts()
+      })
+    },
+    selectWorkout(workout) {
+      let payload = {}
+      switch (this.indexToAddWorkout) {
+        case 0:
+          payload = {
+            mondayWorkoutId: workout.id
+          }
+          break
+        case 1:
+          payload = {
+            tuesdayWorkoutId: workout.id
+          }
+          break
+        case 2:
+          payload = {
+            wednesdayWorkoutId: workout.id
+          }
+          break
+        case 3:
+          payload = {
+            thursdayWorkoutId: workout.id
+          }
+          break
+        case 4:
+          payload = {
+            fridayWorkoutId: workout.id
+          }
+          break
+        case 5:
+          payload = {
+            saturdayWorkoutId: workout.id
+          }
+          break
+        case 6:
+          payload = {
+            sundayWorkoutId: workout.id
+          }
+          break
+      }
+      updatePlan(this.plan.id, payload).then(() => {
+        this.loadPlanWorkouts()
+        this.showAddPlan = false
+        this.indexToAddWorkout = null
+      }).catch((err) => {
+        errorNotify('Une erreur est survenue lors de l\'ajout de votre entrainement au programme')
+        this.loadPlanWorkouts()
+      })
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.draggable-btn {
+  &-container {
+    position: absolute;
+    left: 10px;
+    height: 100%;
+    display: flex;
+    align-items: center;
+  }
+}
+.workout-day {
+  opacity: 0;
+}
+.restDay {
+  opacity: 0;
+}
+.plan-template {
+  width: 100%;
+  top: -20px;
+  left: 0;
+  &__card {
+    outline: 3px dashed $primary;
+    outline-offset: -3px;
+    height: 90px;
+    @media (min-width: 600px) {
+      height: 64px;
+    }
+    &-button {
+      height: 42px;
+    }
+  }
+}
+</style>
