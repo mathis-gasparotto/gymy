eba450d64eee294a343ad40c6ab93a543995ae2e
Mathis Gasparotto <mgasparotto@getinov.com>
2024-03-27 16:27:34 +0100

    :sparkles: Update password

--- src/helpers/translatting.js
+++ src/helpers/translatting.js
@@ -1,6 +1,5 @@
 export default () => ({
-  translateSignupError(error) {
-    console.log(error)
+  translateSignupError(error, defaultMessage = null) {
     if (error.message === "Nom d'utilisateur déjà utilisé") return error.message
     switch (error.message) {
       case 'Firebase: Error (auth/email-already-in-use).':
@@ -8,11 +7,10 @@ export default () => ({
       case 'Firebase: Error (auth/invalid-email).':
         return 'Email invalide'
       default:
-        return 'Une erreur est survenue'
+        return defaultMessage || 'Une erreur est survenue'
     }
   },
-  translateSigninError(error) {
-    console.log(error)
+  translateSigninError(error, defaultMessage = null) {
     switch (error.message) {
       case 'Firebase: Error (auth/wrong-password).':
         return 'Identifiants incorrects'
@@ -23,45 +21,49 @@ export default () => ({
       case 'Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests).':
         return 'Vous avez effectué trop de tentative, veuillez attendre un moment avant de réessayer'
       default:
-        return 'Une erreur est survenue'
+        return defaultMessage || 'Une erreur est survenue'
     }
   },
-  translateLogoutError(error) {
+  translateLogoutError(error, defaultMessage = null) {
     switch (error.message) {
       default:
-        return 'Une erreur est survenue'
+        return defaultMessage || 'Une erreur est survenue'
     }
   },
-  translateDeleteUserError(error) {
+  translateDeleteUserError(error, defaultMessage = null) {
     switch (error.message) {
       default:
-        return 'Une erreur est survenue'
+        return defaultMessage || 'Une erreur est survenue'
     }
   },
-  translateUpdatePasswordError(error) {
+  translateUpdatePasswordError(error, defaultMessage = null) {
     switch (error.message) {
       case 'Firebase: Error (auth/wrong-password).':
         return 'Ancien mot de passe incorrect'
+      case 'Firebase: Error (auth/invalid-credential).':
+        return 'Ancien mot de passe incorrect'
+      case 'FirebaseError: Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests).':
+        return 'Vous avez effectué trop de tentative, veuillez attendre un moment avant de réessayer'
       default:
-        return 'Une erreur est survenue'
+        return defaultMessage || 'Une erreur est survenue'
     }
   },
-  translateUpdateUserError(error) {
+  translateUpdateUserError(error, defaultMessage = null) {
     switch (error.message) {
       default:
-        return 'Une erreur est survenue'
+        return defaultMessage || 'Une erreur est survenue'
     }
   },
-  translateGetUserError(error) {
+  translateGetUserError(error, defaultMessage = null) {
     switch (error.message) {
       default:
-        return 'Une erreur est survenue'
+        return defaultMessage || 'Une erreur est survenue'
     }
   },
-  translateUpdateUserEmailError(error) {
+  translateUpdateUserEmailError(error, defaultMessage = null) {
     switch (error.message) {
       default:
-        return 'Une erreur est survenue'
+        return defaultMessage || 'Une erreur est survenue'
     }
   },
   translateError(error, defaultMessage = null) {
