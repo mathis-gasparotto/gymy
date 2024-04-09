e30c2fe9721719a39b5d1f15686d8e096a238207
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2024-04-09 12:13:25 +0200

    :sparkles: Display today workouts on index page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,8 +2,9 @@
   <q-page class="h-100">
     <div class="page-content flex items-center column">
       <h1 class="text-h4">Bonjour {{ user.username }}</h1>
-      <img alt="Quasar logo" src="~assets/icon.png" style="width: 200px">
-      <Timer class="q-mt-xl" />
+      <!-- <img alt="Gymy logo" src="~assets/icon.png" style="width: 100px"> -->
+      <Timer />
+      <TodayWorkouts class="q-mt-xl" />
     </div>
   </q-page>
 </template>
@@ -11,11 +12,13 @@
 <script>
 import { getUser } from 'src/services/userService'
 import Timer from 'src/components/Timer.vue'
+import TodayWorkouts from 'src/components/TodayWorkouts.vue'
 
 export default {
   name: 'IndexPage',
   components: {
-    Timer
+    Timer,
+    TodayWorkouts
   },
   setup() {
     return {
