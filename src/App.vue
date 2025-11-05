a087baedf498bf05da186c21febffbab5eff51ef
Mathis Gasparotto <mathis.gasparotto@hotmail.com>
2025-11-05 09:43:10 +0100

    feat(darkmode): update status bar color with dark mode

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 <script>
 import { LocalStorage, useQuasar } from 'quasar'
 import { watch } from 'vue'
+import { StatusBar, Style } from '@capacitor/status-bar'
 
 export default {
   name: 'App',
@@ -19,8 +20,28 @@ export default {
       $q.dark.set('auto')
     }
 
+    if ($q.platform.is.capacitor) {
+      if ($q.dark.isActive === true) {
+        StatusBar.setStyle({ style: Style.Dark })
+        StatusBar.setBackgroundColor({ color: '#0e0f0f' })
+      } else {
+        StatusBar.setStyle({ style: Style.Light })
+        StatusBar.setBackgroundColor({ color: '#ffffff' })
+      }
+    }
+
     watch(() => $q.dark.mode, val => {
       LocalStorage.set('darkMode', val)
+
+      if ($q.platform.is.capacitor) {
+        if ($q.dark.isActive === true) {
+          StatusBar.setStyle({ style: Style.Dark })
+          StatusBar.setBackgroundColor({ color: '#0e0f0f' })
+        } else {
+          StatusBar.setStyle({ style: Style.Light })
+          StatusBar.setBackgroundColor({ color: '#ffffff' })
+        }
+      }
     })
 
     return {
